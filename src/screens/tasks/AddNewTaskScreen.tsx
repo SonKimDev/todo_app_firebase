0884a3b2f489f486913e264f9fef3d2bf6efc9a1
sonkh <kimson2507.contact@gmail.com>
2024-12-24 10:03:28 +0700

    feat: add task

--- src/screens/tasks/AddNewTaskScreen.tsx
+++ src/screens/tasks/AddNewTaskScreen.tsx
@@ -1,15 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import Container from '../../components/Container';
-import InputComponent from '../../components/InputComponent';
-import SectionComponent from '../../components/SectionComponent';
-import {TaskModel} from '../../models/TaskModel';
-import {Button, View} from 'react-native';
-import DateTimePickerComponent from '../../components/DateTimePickerComponent';
-import RowComponent from '../../components/RowComponent';
-import SpaceComponent from '../../components/SpaceComponent';
-import DropDownPickerComponent from '../../components/DropDownPickerComponent';
-import {SelectModel} from '../../models/SelectModel';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import {AttachSquare} from 'iconsax-react-native';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import DocumentPicker, {
+  DocumentPickerResponse,
+} from 'react-native-document-picker';
+import ButtonComponent from '../../components/ButtonComponent';
+import Container from '../../components/Container';
+import DateTimePickerComponent from '../../components/DateTimePickerComponent';
+import DropDownPickerComponent from '../../components/DropDownPickerComponent';
+import InputComponent from '../../components/InputComponent';
+import RowComponent from '../../components/RowComponent';
+import SectionComponent from '../../components/SectionComponent';
+import SpaceComponent from '../../components/SpaceComponent';
+import TextComponent from '../../components/TextComponent';
+import TitleComponent from '../../components/TitleComponent';
+import {colors} from '../../constants/colors';
+import {SelectModel} from '../../models/SelectModel';
+import {TaskModel} from '../../models/TaskModel';
+
 const initValue: TaskModel = {
   title: '',
   description: '',
@@ -20,9 +30,12 @@ const initValue: TaskModel = {
   fileUrls: [],
 };
 
-export default function AddNewTaskScreen({navigation}: any) {
+export default function AddNewTaskScreen() {
   const [taskDetail, setTaskDetail] = useState<TaskModel>(initValue);
   const [userSelect, setUserSelect] = useState<SelectModel[]>([]);
+  const [attackments, setAttackments] = useState<DocumentPickerResponse[]>([]);
+  const [attackmentUrl, setAttackmentUrl] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleGetAllUser() {
     await firestore()
@@ -52,8 +65,55 @@ export default function AddNewTaskScreen({navigation}: any) {
     setTaskDetail(item);
   }
 
-  function handleAddNewTask() {
-    console.log(taskDetail);
+  function handlePickerDocument() {
+    DocumentPicker.pick({
+      type: [DocumentPicker.types.allFiles],
+      copyTo: 'cachesDirectory',
+    })
+      .then(res => {
+        setAttackments(res);
+        res.forEach(item => handleUploadFileToStorage(item));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  async function handleUploadFileToStorage(item: DocumentPickerResponse) {
+    try {
+      const filename = item.name
+        ? item.name + `${Date.now()}`
+        : `file${Date.now()}`;
+      const path = `documents/${filename}`;
+      const items = [...attackmentUrl];
+
+      await storage().ref(path).putFile(item.fileCopyUri!);
+
+      const url = await storage().ref(path).getDownloadURL();
+
+      items.push(url);
+      setAttackmentUrl(items);
+    } catch (error) {
+      console.log('Error uploading file:', error);
+    }
+  }
+
+  async function handleAddNewTask() {
+    const data = {
+      ...taskDetail,
+      fileUrls: attackmentUrl,
+    };
+    setIsLoading(true);
+    await firestore()
+      .collection('tasks')
+      .add(data)
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false);
+      });
   }
 
   useEffect(() => {
@@ -61,12 +121,12 @@ export default function AddNewTaskScreen({navigation}: any) {
   }, []);
 
   return (
-    <Container back title="Add New Task">
+    <Container back title="Add New Task" isScroll>
       <SectionComponent>
         <InputComponent
           value={taskDetail.title}
           onChange={val => handleChangeValue('title', val)}
-          title="title"
+          title="Title"
           allowClear
           placeHolder="Title of task"
         />
@@ -114,9 +174,32 @@ export default function AddNewTaskScreen({navigation}: any) {
           multiple
           title="Members"
         />
+
+        <View>
+          <RowComponent
+            justifyContent="flex-start"
+            onPress={handlePickerDocument}>
+            <TitleComponent text="Attackments" />
+            <SpaceComponent width={8} />
+            <AttachSquare size={20} color={colors.white} />
+          </RowComponent>
+          {attackments.length > 0 &&
+            attackments.map((item, index) => (
+              <RowComponent
+                key={index}
+                justifyContent="flex-start"
+                style={{paddingVertical: 12}}>
+                <TextComponent text={item.name ?? ''} />
+              </RowComponent>
+            ))}
+        </View>
       </SectionComponent>
       <SectionComponent>
-        <Button title="Save" onPress={handleAddNewTask} />
+        <ButtonComponent
+          text="Save"
+          onPress={handleAddNewTask}
+          isLoading={isLoading}
+        />
       </SectionComponent>
     </Container>
   );
